93b731bbf24d277ffe8ad27a3c2efe2b3198f545
Максим Суворов <royming06@gmail.com>
2021-08-05 18:08:50 +0300

    example

--- Reminder/Program.cs
+++ Reminder/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace Reminder
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static ITelegramBotClient botClient;
+
+        static void Main() {
+            botClient = new TelegramBotClient("");
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+                              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+                             );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e) {
+            if (e.Message.Text != null)
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                await botClient.SendTextMessageAsync(
+                                                     chatId: e.Message.Chat,
+                                                     text:   "You said:\n" + e.Message.Text
+                                                    );
+            }
         }
     }
 }
